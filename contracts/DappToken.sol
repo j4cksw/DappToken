8abcadeec0be3c1551b91efba9a417609fe25733
Prayoch rujira <prayoch@clockupstudio.com>
2021-06-28 16:06:36 +0700

    Added test for store initialSupply to admin

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,7 +5,10 @@ contract DappToken {
 
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf;
+
     constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 }
--- test/DappToken.spec.js
+++ test/DappToken.spec.js
@@ -7,6 +7,9 @@ contract('DappToken', (accounts) => {
             return tokenInstance.totalSupply();
         }).then((totalSupply) => {
             assert.equal(totalSupply.toNumber(), 1000000, 'Total supply should be 1,000,000 at the beginning.')
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((adminBalance) => {
+            assert.equal(adminBalance.toNumber(), 1000000, 'It should allocate all of the initial supply to admin.')
         });
     });
 });
